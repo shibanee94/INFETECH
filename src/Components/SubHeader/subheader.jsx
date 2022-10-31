50116d8774cf66e9874121030d861c4618f9e9f2
shibanee94 <shibanee.sai94@gmail.com>
2022-11-01 00:59:03 +0530

    added more images

--- src/Components/SubHeader/subheader.jsx
+++ src/Components/SubHeader/subheader.jsx
@@ -36,6 +36,48 @@ const subheader = () => {
           which don't look even slightly believable.
         </p>
       </div>
+      <Container className="projects-img">
+        <div className="p-img1">
+          <h4 className="pic">
+            Tech Solutions
+            <br />
+            <span className="DI">DESIGN/IDES</span>
+          </h4>
+        </div>
+        <div className="p-img2">
+          <h4 className="pic">
+            Smart Visions
+            <br />
+            <span className="DI">DESIGN/IDES</span>
+          </h4>
+        </div>
+        <div className="p-img3">
+          <h4 className="pic">
+            Platform Integration
+            <br />
+            <span className="DI">DESIGN/IDES</span>
+          </h4>
+        </div>
+        <div className="p-img4">
+          <h4 className="pic">
+            Web Development
+            <br />
+            <span className="DI">DESIGN/IDES</span>
+          </h4>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <hr />
+        <div className="sub-footer w-100">
+          <span className="ofc-name">envato</span>
+          <span className="ofc-name">envato</span>
+          <span className="ofc-name">envato</span>
+          <span className="ofc-name">envato</span>
+          <span className="ofc-name">envato</span>
+        </div>
+      </Container>
     </div>
   );
 };
